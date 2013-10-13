4f5344dc57a2fe9a967e8b8db32239e623c8fdc4
Willy Aguirre <marti1125@gmail.com>
2013-10-13 16:46:34 -0500

    se agrega el modelo Usuario

--- SubeAlMetro-Backend/app/models/Estacion.java
+++ SubeAlMetro-Backend/app/models/Estacion.java
@@ -1,9 +1,13 @@
 package models;
 
+import javax.persistence.*;
+
 import play.db.ebean.Model;
 
+@Entity
 public class Estacion extends Model{
-
+	
+	@Id
 	public String codigo;
 	public String nombreEstacion;
 	public String distrito;
